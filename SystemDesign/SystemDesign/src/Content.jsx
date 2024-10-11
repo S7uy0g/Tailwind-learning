dc904b19307e9160b744219455b0f4e0e8d732f2
S7uy0g <RaiJoker103@gmail.com>
2024-10-11 11:16:25 +0545

    Design System

---
+++ SystemDesign/SystemDesign/src/Content.jsx
@@ -0,0 +1,14 @@
+export default function Content() {
+    return (
+      <>
+      <p className="title">This is a title</p>
+      <p className="subtitle">This is a subtitle</p>
+      <p>This is a pargraph</p>
+      <p>This is a anchor</p>
+      <p className="btn">This is a button</p>
+      <p className="btn">This is a button</p>
+      <input type="checkbox"/>
+      <p>Checkbox</p>
+      </>
+    )
+  }
